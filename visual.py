b4265767f88c35b720d535605ef9fea63b2e60a2
Nalla Manish <72727056+19luckyreddy@users.noreply.github.com>
2021-07-24 13:38:05 +0530

    Update visual.py

--- visual.py
+++ visual.py
@@ -3,5 +3,5 @@ import pandas
 
 df = pandas.read_csv('/Users/19luc/OneDrive/Desktop/Test_02/Book1.csv')
 
-pyplot.scatter(df['Age'], df['Salary'])
+pyplot.plot(df['Age'], df['Salary'])
 pyplot.show()
